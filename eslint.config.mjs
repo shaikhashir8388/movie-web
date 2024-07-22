0dc4091e007b33746dbaccebbdd3c2286fadb1c9
Ashir <shaikhashir871@gmail.com>
2024-07-22 17:15:28 +0500

    Add ESLint and configure

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,33 +1,35 @@
+// .eslintrc.mjs
 import js from "@eslint/js";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import next from "@next/eslint-plugin-next";
 
-export default [
-  js.configs.recommended,
-  {
-    files: ["**/*.js", "**/*.jsx"],
-    plugins: {
-      react,
-      reactHooks,
-      next,
-    },
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
+export default {
+  overrides: [
+    {
+      files: ["**/*.js", "**/*.jsx"],
+      plugins: ["react", "react-hooks", "next"],
+      extends: [
+        js.configs.recommended,
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+        "next/core-web-vitals",
+      ],
       parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
         ecmaFeatures: {
           jsx: true,
         },
       },
-    },
-    rules: {
-      // Add your custom rules here
-    },
-    settings: {
-      react: {
-        version: "detect",
+      rules: {
+        // Add your custom rules here
+      },
+      settings: {
+        react: {
+          version: "detect",
+        },
       },
     },
-  },
-];
+  ],
+};
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,33 +1,11 @@
-import js from "@eslint/js";
-import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
-import next from "@next/eslint-plugin-next";
+// postcss.config.mjs
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
-export default [
-  js.configs.recommended,
-  {
-    files: ["**/*.js", "**/*.jsx"],
-    plugins: {
-      react,
-      reactHooks,
-      next,
-    },
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-    },
-    rules: {
-      // Add your custom rules here
-    },
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
-  },
-];
+export default {
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+    // Add other PostCSS plugins here
+  ],
+};
