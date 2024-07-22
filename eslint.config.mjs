31fce864da443dea2bf5ff3ffab84ceb239017f8
Ashir <shaikhashir871@gmail.com>
2024-07-22 15:40:33 +0500

    Add ESLint and configure

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,33 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-import { fixupConfigRules } from "@eslint/compat";
-
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import next from "@next/eslint-plugin-next";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...fixupConfigRules(pluginReactConfig),
+  js.configs.recommended,
+  {
+    files: ["**/*.js", "**/*.jsx"],
+    plugins: {
+      react,
+      reactHooks,
+      next,
+    },
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      // Add your custom rules here
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
 ];
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,33 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-  },
-};
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import next from "@next/eslint-plugin-next";
 
-export default config;
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.js", "**/*.jsx"],
+    plugins: {
+      react,
+      reactHooks,
+      next,
+    },
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      // Add your custom rules here
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+];
